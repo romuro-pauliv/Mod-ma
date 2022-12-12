61f338d967170b1ff32699383336489304c8fb72
Romulo Pauliv <romulopauliv@bk.ru>
2022-12-12 14:28:49 -0300

    modify the __init__.py

---
+++ API/__init__.py
@@ -0,0 +1,46 @@
+# +--------------------------------------------------------------------------------------------------------------------|
+# |                                                                                                    API.__init__.py |
+# |                                                                                             Author: Pauliv, Rômulo |
+# |                                                                                          email: romulopauliv@bk.ru |
+# |                                                                                                    encoding: UTF-8 |
+# +--------------------------------------------------------------------------------------------------------------------|
+
+
+# +--------------------------------------------------------------------------------------------------------------------+
+import os
+from flask import Flask
+from flask_pymongo import PyMongo
+from typing import Union
+# +--------------------------------------------------------------------------------------------------------------------+
+
+
+def create_app(test_config: Union[bool, None] = None) -> Flask:
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        MONGO_URI=os.environ['MONGO_URI']
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+    
+
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+    
+    # A simple page that say hello |-----------------------------------------------------------------------------------|
+    @app.route('/hello')
+    def hello() -> str:
+        return "Hello, World!"
+    # |----------------------------------------------------------------------------------------------------------------|
+
+    return app
+
