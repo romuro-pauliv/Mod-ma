2c455f642ff36251e78deed761d710dfc0dc9cbc
Romulo Pauliv <romulopauliv@bk.ru>
2022-12-13 15:43:04 -0300

    added formatation in __init__.py

--- API/__init__.py
+++ API/__init__.py
@@ -21,12 +21,14 @@ def create_app(test_config: Union[bool, None] = None) -> Flask:
         MONGO_URI=os.environ['MONGO_URI']
     )
 
+    # test config |----------------------------------------------------------------------------------------------------|
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
+    # |----------------------------------------------------------------------------------------------------------------|
 
     # Ensure the instance folder exists |------------------------------------------------------------------------------|
     try:
