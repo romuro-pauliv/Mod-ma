7a68b116db0e272a984c74bd4b5c72db9f0e28d9
Romulo Pauliv <romulopauliv@bk.ru>
2022-12-13 15:42:19 -0300

    added db instance in Flask application

--- API/__init__.py
+++ API/__init__.py
@@ -10,7 +10,6 @@
 import os
 from flask import Flask
 from typing import Union
-from flask_pymongo import PyMongo
 # +--------------------------------------------------------------------------------------------------------------------+
 
 
@@ -22,6 +21,13 @@ def create_app(test_config: Union[bool, None] = None) -> Flask:
         MONGO_URI=os.environ['MONGO_URI']
     )
 
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
     # Ensure the instance folder exists |------------------------------------------------------------------------------|
     try:
         os.makedirs(app.instance_path)
@@ -30,10 +36,16 @@ def create_app(test_config: Union[bool, None] = None) -> Flask:
     # |----------------------------------------------------------------------------------------------------------------|
     
     # A simple page that say hello |-----------------------------------------------------------------------------------|
-    print("Hello Route")
     @app.route('/hello')
     def hello() -> str:
         return os.environ["MONGO_URI"]
     # |----------------------------------------------------------------------------------------------------------------|
+
+    from . import db
+
+    @app.route("/db")
+    def testing() -> str:
+        return db.get_db().list_database_names()
+
 
     return app
