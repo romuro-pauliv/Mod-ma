77971eb740550cb7eb5ab032f3a4ac2ca676e15a
Romulo Pauliv <romulopauliv@bk.ru>
2023-02-13 18:21:26 -0300

    added new tests in token

--- tests/auth/test_token.py
+++ tests/auth/test_token.py
@@ -10,43 +10,11 @@ import requests
 import time
 import jwt
 from werkzeug.security import generate_password_hash
-from typing import Union
 import datetime
 from config import *
+from typing import Any
 # |--------------------------------------------------------------------------------------------------------------------|
 
-# INTIAL CONFIG TO TEST |==============================================================================================|
-def test_pre_test_delete_admin_login() -> None:
-    admin_user: dict[str] = mongo.USERS.REGISTER.find({"username":"admin"})
-
-    for document in admin_user:
-        try:
-            if document['username'] == "admin":
-                assert document['username'] == "admin"
-                mongo.USERS.REGISTER.delete_one({"username":"admin"})
-        except KeyError:
-            pass
-
-
-# |====================================================================================================================|
-# REAL REGISTER |======================================================================================================|
-# |====================================================================================================================|
-def test_real_register() -> None:
-    # + header build +
-    header: dict[str] = {"Register": header_base64_register("admin", "123!Admin", "admin@admin.com")}
-
-    # + request +
-    rtn = requests.post(f"{root_route}{register_route}", headers=header)
-
-    # + tests +
-    assert rtn.text == "CREATED"
-    assert rtn.status_code == 201
-# |====================================================================================================================|
-
-
-# |====================================================================================================================|
-# | REAL TOKEN |=======================================================================================================|
-# |====================================================================================================================|
 def test_real_token() -> None:
     token: str = token_return("admin", "123!Admin")
     # + header +
@@ -60,9 +28,6 @@ def test_real_token() -> None:
     assert rtn.status_code == 202
 
 
-# |====================================================================================================================|
-# | WITHOUT TOKEN |====================================================================================================|
-# |====================================================================================================================|
 def test_without_token() -> None:
     # + header +
     header: dict[None] = {}
@@ -71,13 +36,10 @@ def test_without_token() -> None:
     rtn = requests.post(f"{root_route}{test_token_route}", headers=header)
 
     # + tests +
-    assert rtn.text == "BAD REQUEST - NO DATA"
+    assert json.loads(rtn.text)["response"] == "A STRING WAS NOT IDENTIFIED IN THE TOKEN"
     assert rtn.status_code == 400
 
 
-# |====================================================================================================================|
-# | EXPIRED SIGNATURE TOKEN |==========================================================================================|
-# |====================================================================================================================|
 def test_expired_signature_token() -> None:
     token: str = token_return("admin", "123!Admin")
     # + header +
@@ -88,13 +50,10 @@ def test_expired_signature_token() -> None:
     rtn = requests.post(f"{root_route}{test_token_route}", headers=header)
 
     # + tests +
-    assert rtn.text == "EXPIRED TOKEN"
+    assert json.loads(rtn.text)["response"] == "EXPIRED TOKEN"
     assert rtn.status_code == 403
 
 
-# |====================================================================================================================|
-# | INVALID TOKEN |====================================================================================================|
-# |====================================================================================================================|
 def test_invalid_token() -> None:
     token: str = token_return("admin", "123!Admin")
     token: str = token + "a"
@@ -105,13 +64,10 @@ def test_invalid_token() -> None:
     rtn = requests.post(f"{root_route}{test_token_route}", headers=header)
 
     # + tests +
-    assert rtn.text == "INVALID TOKEN"
-    assert rtn.status_code == 400
+    assert json.loads(rtn.text)["response"] == "INVALID TOKEN"
+    assert rtn.status_code == 403
 
 
-# |====================================================================================================================|
-# | WRONG FORMATTING TOKEN |===========================================================================================|
-# |====================================================================================================================|
 def test_wrong_formatting_token() -> None:
     token: str = token_return("admin", "123!Admin")
     # + header +
@@ -121,13 +77,10 @@ def test_wrong_formatting_token() -> None:
     rtn = requests.post(f"{root_route}{test_token_route}", headers=header)
 
     # + tests +
-    assert rtn.text == "INVALID TOKEN"
-    assert rtn.status_code == 400
+    assert json.loads(rtn.text)["response"] == "INVALID TOKEN"
+    assert rtn.status_code == 403
 
 
-# |====================================================================================================================|
-# | FALSE IP ADDRESS |=================================================================================================|
-# |====================================================================================================================|
 def test_false_ip_address_token() -> None:
     # + generate false token with false ip address +
     false_ip: str = "142.250.219.206"
@@ -146,13 +99,10 @@ def test_false_ip_address_token() -> None:
     rtn = requests.post(f"{root_route}{test_token_route}", headers=header)
 
     # + tests +
-    assert rtn.text == "IP ADDRESS DOES NOT MATCH"
+    assert json.loads(rtn.text)["response"] == "IP ADDRESS DOES NOT MATCH"
     assert rtn.status_code == 403
 
 
-# |====================================================================================================================|
-# | COLON ERROR |======================================================================================================|
-# |====================================================================================================================|
 def test_colon_error() -> None:
     token: str = token_return("admin", "123!Admin")
     # + header +
@@ -162,5 +112,5 @@ def test_colon_error() -> None:
     rtn = requests.post(f"{root_route}{test_token_route}", headers=header)
 
     # + tests +
-    assert rtn.text == "BAD REQUEST - COLON ERROR"
+    assert json.loads(rtn.text)["response"] == "WAS NOT IDENTIFIED [:] COLON IN THE CREDENTIALS"
     assert rtn.status_code == 400
