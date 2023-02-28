f5ce7e08d92a77a8d2f7e88953d19da332311fb3
Romulo Pauliv <romulopauliv@bk.ru>
2023-02-28 09:22:59 -0300

    init iam tests

---
+++ tests/iamtests/config.py
@@ -0,0 +1,62 @@
+# +--------------------------------------------------------------------------------------------------------------------|
+# |                                                                                                 test.iam.config.py |
+# |                                                                                             Author: Pauliv, Rômulo |
+# |                                                                                          email: romulopauliv@bk.ru |
+# |                                                                                                    encoding: UTF-8 |
+# +--------------------------------------------------------------------------------------------------------------------|
+
+# | imports |----------------------------------------------------------------------------------------------------------|
+import os
+import json
+import base64
+import requests
+
+from pymongo import MongoClient
+from pathlib import Path
+from dotenv import load_dotenv
+# |--------------------------------------------------------------------------------------------------------------------|
+
+# ROUTES |=============================================================================================================|
+root_route: str = "http://127.0.0.1:5000"
+login_route: str = "/auth/login"
+register_route: str = "/auth/register"
+iam_route: str = "/auth/iam"
+
+database: str = "/database/"
+collection: str = "/collection/"
+document: str = "/document/"
+# |====================================================================================================================|
+
+# MONGO CLIENT |=======================================================================================================|
+json_path: str = Path("tests/path.json")
+with open(json_path, 'r') as js_file:
+    json_dt: dict[str] = json.load(js_file)
+    js_file.close()
+load_dotenv(dotenv_path=Path(json_dt['dotenv']))
+
+mongo = MongoClient(os.getenv('MONGO_URI'))
+# |====================================================================================================================|
+
+
+# PRE FUNCTION |-------------------------------------------------------------------------------------------------------|
+def header_base64_login(username: str, password: str) -> str:
+    encode_pass: bytes = f"{username}:{password}".encode()
+    return f"Basic {base64.b64encode(encode_pass).decode()}"
+
+
+def header_base64_register(username: str, password: str, email: str) -> str:
+    encode_register: bytes = f"{username}:{password}:{email}".encode()
+    return f"Basic {base64.b64encode(encode_register).decode()}"
+
+
+def token_login(username: str, password: str) -> str:
+    header: dict[str] = {"Authorization": header_base64_login(username, password)}
+    return json.loads(requests.post(f"{root_route}{login_route}", headers=header).text)['token']
+
+def get_id(filter: dict[str], database: str, collection: str) -> str:
+    _id_doc_iterable: str = mongo[database][collection].find(filter)
+    _id_doc: str = ""
+    for doc in _id_doc_iterable:
+        _id_doc: str = doc['_id']
+    return _id_doc
+# |--------------------------------------------------------------------------------------------------------------------|
---
+++ tests/iamtests/test_post.py
@@ -0,0 +1,75 @@
+# +--------------------------------------------------------------------------------------------------------------------|
+# |                                                                                     test.iam.require_privileges.py |
+# |                                                                                             Author: Pauliv, Rômulo |
+# |                                                                                          email: romulopauliv@bk.ru |
+# |                                                                                                    encoding: UTF-8 |
+# +--------------------------------------------------------------------------------------------------------------------|
+
+# | Imports |----------------------------------------------------------------------------------------------------------|
+import requests
+from config import *
+from typing import Any
+# |--------------------------------------------------------------------------------------------------------------------|
+
+# | Parameters |-------------------------------------------------------------------------------------------------------|
+credentials: dict[str] = {"username": "iamtest", "password": "123!IamTest", "email": "iamtest@iamtest.com"}
+register_base64: str = header_base64_register(credentials['username'], credentials['password'], credentials['email'])
+requests.post(f"{root_route}{register_route}", headers={"Register": register_base64})
+header: dict[str] = {"Authorization": f"Bearer {token_login(credentials['username'], credentials['password'])}"}
+# |--------------------------------------------------------------------------------------------------------------------|
+
+# | Functions |--------------------------------------------------------------------------------------------------------|
+def response_assert(hypothetical_response: str, request_obj: requests.models.Response) -> bool:
+    return (hypothetical_response == json.loads(request_obj.text)['response'])
+
+def status_code_assert(hypothetical_status_code: int, request_obj: requests.models.Response) -> bool:
+    return (hypothetical_status_code == request_obj.status_code)
+# |--------------------------------------------------------------------------------------------------------------------|
+
+# | Test database post |-----------------------------------------------------------------------------------------------|
+def test_database_post() -> None:
+    send_json: dict[str] = {"database": "iamtesting"}
+    response: requests.models.Response = requests.post(f"{root_route}{database}", headers=header, json=send_json)
+    
+    assert response_assert(f"USER [{credentials['username']}] REQUIRE PRIVILEGES", response)
+    assert status_code_assert(403, response)
+# |--------------------------------------------------------------------------------------------------------------------|
+
+# | Test collection post |---------------------------------------------------------------------------------------------|
+def test_collection_post() -> None:
+    database_name_list: list[str] = mongo.list_database_names()
+    
+    for database_name in database_name_list:
+        send_json: dict[str] = {"database": database_name, "collection": "testing"}
+        response: requests.models.Response = requests.post(f"{root_route}{collection}", headers=header, json=send_json)
+        
+        if len(database_name) <= 4:
+            assert response_assert(f"THE INFORMED NAME [{database_name}] MUST BE MORE THAN [4] CHARACTERS", response)
+            assert status_code_assert(400, response)
+        else:
+            assert response_assert(f"USER [{credentials['username']}] REQUIRE PRIVILEGES", response)
+            assert status_code_assert(403, response)
+# |--------------------------------------------------------------------------------------------------------------------|
+
+# | Testing document post |--------------------------------------------------------------------------------------------|
+def test_document_post() -> None:
+    database_name_list: list[str] = mongo.list_database_names()
+    
+    for database_name in database_name_list:
+        collection_name_list: list[str] = mongo[database_name].list_collection_names()
+        
+        for collection_name in collection_name_list:
+            json_send: dict[str] = {
+                "database": database_name, "collection": collection_name, "document": {"testing": "mode"}
+            }
+            
+            response: requests.models.Response = requests.post(
+                f"{root_route}{document}", headers=header, json=json_send)
+            
+            if database_name in ["admin", "config", "local"]:
+                assert response_assert(f"DATABASE [{database_name}] NOT FOUND", response)
+                assert status_code_assert(404, response)
+            else:
+                assert response_assert(f"USER [{credentials['username']}] REQUIRE PRIVILEGES", response)
+                assert status_code_assert(403, response)
+# |--------------------------------------------------------------------------------------------------------------------|
