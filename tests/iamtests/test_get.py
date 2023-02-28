d84de7bdfcd5dafc9ebcecb3cf2ac7e693eedc64
Romulo Pauliv <romulopauliv@bk.ru>
2023-02-28 13:09:07 -0300

    added test get in iam tests

--- tests/iamtests/test_get.py
+++ tests/iamtests/test_get.py
@@ -16,6 +16,7 @@ credentials: dict[str] = {"username": "iamtest", "password": "123!IamTest", "ema
 register_base64: str = header_base64_register(credentials['username'], credentials['password'], credentials['email'])
 requests.post(f"{root_route}{register_route}", headers={"Register": register_base64})
 header: dict[str] = {"Authorization": f"Bearer {token_login(credentials['username'], credentials['password'])}"}
+privileges: dict[str, list | dict] = mongo.USERS.PRIVILEGES.find_one({"command": "privileges"})
 # |--------------------------------------------------------------------------------------------------------------------|
 
 # | Functions |--------------------------------------------------------------------------------------------------------|
@@ -26,3 +27,37 @@ def status_code_assert(hypothetical_status_code: int, request_obj: requests.mode
     return (hypothetical_status_code == request_obj.status_code)
 # |--------------------------------------------------------------------------------------------------------------------|
 
+
+# | Test Read Database |-----------------------------------------------------------------------------------------------|
+def test_read_database() -> None:
+    response: requests.models.Response = requests.get(f"{root_route}{database}", headers=header)
+    if credentials['username'] in privileges['database']['read']:
+        status_code_assert(200, response)
+# |--------------------------------------------------------------------------------------------------------------------|
+
+# | Test Read Collection |---------------------------------------------------------------------------------------------|
+def test_read_collection() -> None:
+    for database_name in mongo.list_database_names():
+        send_json: dict[str] = {"database": database_name}
+        response: requests.models.Response = requests.get(f"{root_route}{collection}", headers=header, json=send_json)
+        
+        if credentials['username'] in privileges['collection']['read']:
+            status_code_assert(200, response)
+# |--------------------------------------------------------------------------------------------------------------------|
+
+# | Test Read Documents |----------------------------------------------------------------------------------------------|
+def test_read_documents() -> None:
+    for database_name in mongo.list_database_names():
+        for collection_name in mongo[database_name].list_collection_names():
+            send_json: dict[str] = {"database": database_name, "collection": collection_name, "filter": {}}
+            response: requests.models.Response = requests.get(f"{root_route}{document}", headers=header, json=send_json)
+            
+            if database_name in ["config", "admin", "local"]:
+                assert response_assert(f"DATABASE [{database_name}] NOT FOUND", response)
+                assert status_code_assert(404, response)
+            elif credentials["username"] not in privileges[database_name][collection_name]['read']:
+                assert response_assert(f"USER [{credentials['username']}] REQUIRE PRIVILEGES", response)
+                assert status_code_assert(403, response)
+            elif credentials['username'] in privileges[database_name][collection_name]['read']:
+                assert status_code_assert(200, response)
+# |--------------------------------------------------------------------------------------------------------------------|
